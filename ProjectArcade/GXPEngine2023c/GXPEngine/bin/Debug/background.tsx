f458bdc5afcbf7e27d6eba1530b724e3a429d2f3
chocoletter <151574354+chocoletter@users.noreply.github.com>
2024-02-29 15:00:02 +0100

    Clear repository with .gitignore

--- ProjectArcade/GXPEngine2023c/GXPEngine/bin/Debug/background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="background" tilewidth="32" tileheight="32" tilecount="1008" columns="42">
- <image source="pictures/background.jpeg" width="1366" height="768"/>
-</tileset>
